95261c4e32303184ee621ab2580572c8dc1a5927
giseon <pakox123@gmail.com>
2024-12-28 19:00:26 +0900

    feat : 공통헤더제작(링크)

--- section2/pages/_app.tsx
+++ section2/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+  <header>
+  <a href={'/'}>index</a>
+  %nbsp
+  <Link href={'/book/3'}>book/3</Link>
+  %nbsp
+  <Link href={'/search'}>search</Link>
+  </header>
+  <Component {...pageProps} />;
+  </>
 }
