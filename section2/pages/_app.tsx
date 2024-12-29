0320155e49ccf7b0b26f7877ff95d8f3a627a743
giseon <pakox123@gmail.com>
2024-12-30 01:26:51 +0900

    초기파일 설정

--- section2/pages/_app.tsx
+++ section2/pages/_app.tsx
@@ -1,16 +1,25 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import Link from "next/link";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <>
-  <header>
-  <a href={'/'}>index</a>
-  %nbsp
-  <Link href={'/book/3'}>book/3</Link>
-  %nbsp
-  <Link href={'/search'}>search</Link>
-  </header>
-  <Component {...pageProps} />;
-  </>
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  const getLayout =
+    Component.getLayout ?? ((page: ReactNode) => page);
+
+  return (
+    <GlobalLayout>
+      {getLayout(<Component {...pageProps} />)}
+    </GlobalLayout>
+  );
 }
