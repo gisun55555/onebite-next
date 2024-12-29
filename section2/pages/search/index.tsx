93544dab8518e1ee609e1307b7cf6437930013f2
giseon <pakox123@gmail.com>
2024-12-30 01:27:03 +0900

    feat : ssr페이지 구현

--- section2/pages/search/index.tsx
+++ section2/pages/search/index.tsx
@@ -1,9 +1,19 @@
-import { useRouter } from "next/router"
+import SearchableLayout from "@/components/searchable-layout";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
-export default function page(){
-
-    const router = useRouter()
-    const {q} = router.query
-    console.log(router)
-    return <h1>Search{q}</h1>
+export default function Page() {
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
