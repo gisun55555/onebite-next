3fdc0db3847a0c9ae3f8c429d4e18bc646942ca0
giseon <pakox123@gmail.com>
2024-12-30 01:25:51 +0900

    feat : 초기 ui 설정

--- section2/pages/index.tsx
+++ section2/pages/index.tsx
@@ -1,4 +1,53 @@
+// CSS Module
+import SearchableLayout from "@/components/searchable-layout";
+import style from "./index.module.css";
+import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
-  return <h1>인덱스페이지</h1>
+
+export const getServerSideProps = async ()=>{
+  const allBooks = await fetchBooks()
+  const recoBooks = await fetchRandomBooks()
+
+
+
+  return{
+    props:{
+      allBooks,
+      recoBooks,
+    },
+  }
 }
+
+
+export default function Home({
+allBooks,
+recoBooks,
+}:InferGetServerSidePropsType<typeof getServerSideProps> ) {
+
+  return (
+    
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {recoBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {allBooks.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
